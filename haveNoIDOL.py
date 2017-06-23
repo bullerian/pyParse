008996d331f43c461b2b48862ea67a5e1dd61795
lizometrix <you@example.com>
2017-06-23 12:32:06 +0300

    Comments added

--- haveNoIDOL.py
+++ haveNoIDOL.py
@@ -14,11 +14,28 @@ generalRules = [lambda x: len(x)]
 test = list()
 
 def getfile(path):
+    """
+    Open file in readonly mode, save file descriptor
+    to inFile global var
+    :param path: path to file
+    :return: none
+    """
     global inFile
     inFile = open(path, 'r')
 
 
 def split_stream(fs, delim='\n', bufsize=1024):
+    """
+    Splits stream of data by delim.
+    Reads buffsize bytes per one call.
+    Yields delimited string
+    :param fs: file descriptor
+    :param delim: delimiter variable
+    :param bufsize: size of read buffer
+    :return: if delimiter is found yields splited string
+            or empty string if it's not found
+    """
+
     prev = ''
     while True:
         s = fs.read(bufsize)
@@ -37,31 +54,49 @@ def split_stream(fs, delim='\n', bufsize=1024):
 
 
 def sort(rcv, misfitRcv, gnrRule):
+    """
+
+    :param rcv:
+    :param misfitRcv:
+    :param gnrRule:
+    :return:
+    """
     pdict = dict()
-    pdict[misfitRcv.pop()]
+
+    isAllRulesApply=True
 
     for packet in split_stream(inFile):
         for gnr in gnrRule:
-            if gnr(packet):
+            # proceed if generall
+            if not gnr(packet):
+                continue
                 for name, rule in rcv:
-
+                    # add empty list as new key in dictionary
+                    # key == rule name
                     if not pdict.has_key(name):
                         pdict[name]=list()
 
-                    #isRulestrue = True
-
+                    # check packet against all rules
+                    # if one rule failes abort
                     for predicate in rule:
                         if not (predicate(packet)):
-                            #isRulestrue = False
-                            continue
-#                    if isRulestrue:
-                    pdict[name].append(packet)
-                        #isRulestrue = True
+                            isAllRulesApply=False
+                            break
+
+                    if isAllRulesApply:
+                        pdict[name].append(packet)
 
     return pdict
 
 
 def save_files(filledDict):
+    """
+    Creates one file per every key in dictionary.
+    Writes all of dicts value items as separate lines
+    to file named as key + FILE_NAME_SUFFIX
+    :param filledDict: dictionary that contains names as keys and lists of strings as values
+    :return: none
+    """
 
     for key, list in filledDict.iteritems():
         f=open(key+FILE_NAME_SUFFIX, 'w+')
@@ -71,7 +106,7 @@ def save_files(filledDict):
 
 def main():
     getfile('messages.txt')
-    save_files(sort(receivers, misfit))
+    save_files(sort(receivers, misfit, generalRules))
 
 
 if __name__ == '__main__':
