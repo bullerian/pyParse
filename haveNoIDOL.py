aecddd3758346a824b9e09d0c9315314d983fc34
lizometrix <you@example.com>
2017-06-23 15:18:10 +0300

    almost working

--- haveNoIDOL.py
+++ haveNoIDOL.py
@@ -1,92 +1,72 @@
-inFile = ''
 FILE_NAME_SUFFIX = ".txt"
 
 Irule = [lambda x: (len(x) % 2)]
 Drule = [lambda x: ((len(x) % 2) is 1), lambda x: x.split()[0].istitle()]
-Orule = list()
+Orule = [lambda x: True]
 Lrule = [lambda x: x.split()[-1].lower() == 'end']
 
 receivers = [("ivan", Irule), ("dmytro", Drule), ("lesia", Lrule)]
-misfit =[("ostap", Orule)]
+misfit =("ostap", Orule)
+
 
 generalRules = [lambda x: len(x)]
 
 test = list()
 
-def getfile(path):
-    """
-    Open file in readonly mode, save file descriptor
-    to inFile global var
-    :param path: path to file
-    :return: none
-    """
-    global inFile
-    inFile = open(path, 'r')
-
-
-def split_stream(fs, delim='\n', bufsize=1024):
-    """
-    Splits stream of data by delim.
-    Reads buffsize bytes per one call.
-    Yields delimited string
-    :param fs: file descriptor
-    :param delim: delimiter variable
-    :param bufsize: size of read buffer
-    :return: if delimiter is found yields splited string
-            or empty string if it's not found
+def sort(rawDataList, rcv, misfitRcv, gnrRule):
     """
 
-    prev = ''
-    while True:
-        s = fs.read(bufsize)
-        if not s:
-            break
-        split = s.split(delim)
-        if len(split) > 1:
-            yield prev + split[0]
-            prev = split[-1]
-            for x in split[1:-1]:
-                yield x
-        else:
-            prev += s
-    if prev:
-        yield prev
 
-
-def sort(rcv, misfitRcv, gnrRule):
-    """
-
+    :param source:
     :param rcv:
     :param misfitRcv:
-    :param gnrRule:
+    :param gnrRule: general rule that all packets must apply
     :return:
     """
     pdict = dict()
 
-    isAllRulesApply=True
+    # set misfit variables if misfit parameters are provided
+    if misfitRcv:
+        misfitName = misfitRcv[0]
+        misfitList = list()
+        pdict[misfitName]=[]
 
-    for packet in split_stream(inFile):
-        for gnr in gnrRule:
-            # proceed if generall
-            if not gnr(packet):
-                continue
-                for name, rule in rcv:
-                    # add empty list as new key in dictionary
-                    # key == rule name
-                    if not pdict.has_key(name):
-                        pdict[name]=list()
+    for packet in rawDataList:
+        
+        if not isRulesApplyToPacket(gnrRule, packet):
+            continue
+            
+        pTaken = 0
+        
+        for name, rule in rcv:
+            # add empty list as new key in dictionary
+            # key == rule name
+            if name not in pdict:
+                pdict[name]=list()
 
-                    # check packet against all rules
-                    # if one rule failes abort
-                    for predicate in rule:
-                        if not (predicate(packet)):
-                            isAllRulesApply=False
-                            break
-
-                    if isAllRulesApply:
-                        pdict[name].append(packet)
+            # check packet against all rules
+            # if one rule failes abort
+            if isRulesApplyToPacket(rule, packet):
+                pdict[name].append(packet)
+                pTaken=pTaken+1
+        
+        # if misfit packet has receiver
+        # else misfit packet will be dropped
+        if not pTaken and misfitRcv:
+            # if misfits packet has rules they can be checked here
+            misfitList.append(packet)
 
     return pdict
+
+
+def isRulesApplyToPacket(ruleList, packet):
+    """
+    Check if packet satisfies all rules
+    :param ruleList: list of rules
+    :param packet: string inspect
+    :return: 
+    """
+    return all(x(packet) for x in ruleList)
 
 
 def save_files(filledDict):
@@ -104,9 +84,17 @@ def save_files(filledDict):
         f.writelines(["%s\n" % item  for item in list])
 
 
-def main():
-    getfile('messages.txt')
-    save_files(sort(receivers, misfit, generalRules))
+def get_splited_file(pth, delim = '\n'):
+    fileDescriptor = open(pth, 'r')
+
+    return fileDescriptor.read().split(delim)
+
+
+def main(path, delimiter):
+    splitedList=get_splited_file(path, delimiter)
+    
+
+    save_files(sort(splitedList, receivers, misfit, generalRules))
 
 
 if __name__ == '__main__':
