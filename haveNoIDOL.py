7fc72892cecfea60e9ae0efe77bbf52145c108d1
Dmytro <lizometrix@gmail.com>
2017-07-03 18:42:09 +0300

    Names changed according to PIP8 style, spelling fixes, names of variables changed

--- haveNoIDOL.py
+++ haveNoIDOL.py
@@ -13,90 +13,101 @@ FIRST_LETTER_UPPERCASE = lambda x: x.split()[0][0].isupper()
 END_IS_LAST_WORD = lambda x: x.split()[-1].lower() == 'end'
 LEN_GT_ZERO = lambda x: len(x)
 
-Irule = [EVEN_SYMBOL_COUNT]
-Drule = [ODD_SYMBOL_COUNT, FIRST_LETTER_UPPERCASE]
-Orule = []
-Lrule = [END_IS_LAST_WORD]
+ivan_rule = [EVEN_SYMBOL_COUNT]
+dmytro_rule = [ODD_SYMBOL_COUNT, FIRST_LETTER_UPPERCASE]
+orest_rule = []
+lesia_rule = [END_IS_LAST_WORD]
 
-receivers = [("ivan", Irule), ("dmytro", Drule), ("lesia", Lrule)]
-misfit = ("ostap", Orule)
+receivers = [("ivan", ivan_rule),
+             ("dmytro", dmytro_rule),
+             ("lesia", lesia_rule)]
+misfit = ("ostap", orest_rule)
 
-generalRules = [LEN_GT_ZERO]
+general_rule = [LEN_GT_ZERO]
 
 
-def sort(rawDataList, rcv, misfitRcv=None, gnrRule=None):
+def sort(raw_data_list, receiver_list, misfit_rcv=None, global_rule=None):
     """
-    Processes string list and adds items to dictionary based on provided predicates in rule list.
+    Processes packet list and adds items to dictionary based on provided
+     predicates in rule list.
     Also applies rejection based on general rule, if it is provided.
     Drops (by default) packets if they did't satisfy any rule.
     Will append items rejected by all rules to misfitsRcv if it is provided.
-    :param rawDataList: list of input strings
-    :param rcv: receivers list that contains tuple in form ("name", [rule list])
-    :param misfitRcv: tuple in form ("name", [rule list]) for packet that doesn't match any rule in rcv list
-    :param gnrRule: general rule that all packets must apply
+    :param raw_data_list: list of input packets
+    :param receiver_list: receivers list that contains tuple in form
+     ("name", [rule list])
+    :param misfit_rcv: tuple in form ("name", [rule list]) for packet that
+     doesn't match any rule in rcv list
+    :param global_rule: general rule that all packets must apply
     :return: dict with names as keys and list of packets as value of keys
             if misfit is present than its packets will be added to dict
     """
-    pdict = dict()
+    packet_dict = dict()
 
     # set misfit variables if misfit parameters are provided
-    if misfitRcv:
-        misfitName = misfitRcv[0]
-        pdict[misfitName] = list()
+    if misfit_rcv:
+        misfitName = misfit_rcv[0]
+        packet_dict[misfitName] = list()
 
-    for packet in rawDataList:
+    for packet in raw_data_list:
 
-        if gnrRule and not isRulesApplyToPacket(gnrRule, packet):
+        if global_rule and not is_rules_apply(global_rule, packet):
             continue
 
-        pTaken = 0
+        packet_taken = 0
 
-        for name, rule in rcv:
+        for name, rule in receiver_list:
             # add empty list as new key in dictionary
             # key == rule name
-            if name not in pdict:
-                pdict[name] = list()
+            if name not in packet_dict:
+                packet_dict[name] = list()
 
             # check packet against all rules
-            # if one rule failes abort
-            if isRulesApplyToPacket(rule, packet):
-                pdict[name].append(packet)
-                pTaken += 1
+            # if one rule fails abort
+            if is_rules_apply(rule, packet):
+                packet_dict[name].append(packet)
+                packet_taken += 1
 
         # if misfit packet has receiver
         # else misfit packet will be dropped
-        if not pTaken and misfitRcv:
+        if misfit_rcv and not packet_taken:
             # if misfits packet has rules they can be checked here
-            pdict[misfitName].append(packet)
+            packet_dict[misfitName].append(packet)
 
-    return pdict
+    return packet_dict
 
 
-def isRulesApplyToPacket(ruleList, packet):
+def is_rules_apply(rule_list, packet):
     """
     Check if packet satisfies all rules
-    :param ruleList: list of rules
-    :param packet: string inspect
-    :return: 
+    :param rule_list: list of rules
+    :param packet: packet to inspect
+    :return: true or false
     """
-    return all(x(packet) for x in ruleList)
+    return all(x(packet) for x in rule_list)
 
 
-def save_files(filledDict):
+def save_files(filled_dict):
     """
     Creates one file per every key in dictionary.
     Writes all of dicts value items as separate lines
     to file named as key + FILE_NAME_SUFFIX
-    :param filledDict: dictionary that contains names as keys and lists of strings as values
+    :param filled_dict: dictionary that contains names as keys and lists of
+     strings as values
     :return: none
     """
-    for key, lst in filledDict.iteritems():
+    for key, lst in filled_dict.iteritems():
         with (open(key + FILE_NAME_SUFFIX, 'w+')) as out_file:
             out_file.writelines(["%s\n" % item for item in lst])
 
 
-
 def get_splited_file(pth, delim):
+    """
+    Opens file and splits it to list of packets by provided delimiter
+    :param pth: path to file
+    :param delim: delimiter string
+    :return: list of packets for processing
+    """
     with (open(pth, 'r')) as in_file:
         line_list = in_file.read().split(delim)
 
@@ -105,16 +116,16 @@ def get_splited_file(pth, delim):
 
 def main(path, delimiter):
     """
-    Splits lines by delimiter, then sorts them based on rule set and adds them to dictionary.
-    Then sorted line lists get saved to separate files.
-    For now rules are hard-coded, but script can be modified to accept separate rule list.
-    Written and tested on Python 2.7.12
+    Splits lines by delimiter, then sorts them based on rule set and adds them
+     to dictionary. Then sorted line lists get saved to separate files.
+    For now rules are hard-coded, but script can be modified to accept separate
+     rule list. Written and tested on Python 2.7.12
     :param path: path to input text file
     :param delimiter: delimiter for line splitting
-    :return: 0 on success or -1 on failure
+    :return: SUCCESS_RETVAL on success or ERROR_RETVAL on failure
     """
     try:
-        splitedList = get_splited_file(path, delimiter)
+        splited_list = get_splited_file(path, delimiter)
     except IOError as e:
         print('Unable to open input file. Error message: ' + e.message)
         return ERROR_RETVAL
@@ -122,10 +133,10 @@ def main(path, delimiter):
         print('Unknown error occurred')
         return ERROR_RETVAL
 
-    processedPacketDict = sort(splitedList, receivers, misfit, generalRules)
+    processed_packet_dict = sort(splited_list, receivers, misfit, general_rule)
 
     try:
-        save_files(processedPacketDict)
+        save_files(processed_packet_dict)
     except IOError as e:
         print('Unable to save processed file(s). Error message: ' + e.message)
         return ERROR_RETVAL
@@ -139,7 +150,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Rule-based sorting')
 
     # Optional delimiter arg
-    parser.add_argument("-d", "--delimiter", type=str, help='Delimiter string ("\\n" by default)')
+    parser.add_argument("-d",
+                        "--delimiter",
+                        type=str,
+                        help='Delimiter string ("\\n" by default)')
     parser.add_argument("path", type=str, help="Path to input file")
 
     args = parser.parse_args()
