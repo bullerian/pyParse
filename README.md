1e93ecf31e2c6b986a2ad1ce4831c4bb3d92141c
Dmytro <lizometrix@gmail.com>
2017-07-08 03:10:01 +0300

    Docstrings added, README.md modified

--- README.md
+++ README.md
@@ -1,12 +1,17 @@
-Script for the IDOL task
+Improved script for second IDOL task
 
-Splits lines by delimiter, then sorts them based on rule set and adds them to dictionary. Can reject lines by general rule. Also it can save so called 'misfit' lines (the ones that doesn't fit any rule). Then sorted line lists get saved to separate files in current directory.
-    For now rules are hard-coded, but script can be modified to accept separate rule list.
-Written and tested on Python 2.7.12
+#TODO: add description
+Sort packets with regexp and transmit them over network using UDP.
+Written and tested on Python 2.7
+
+    Required external modules:
+        scapy
+        argparse
 
 
-USAGE: python haveNoIDOL.py [-h] [-d DELIMITER] path
+USAGE: sudo python udp_idol.py [-h] [-t timeout] path servers
 
     -h   -- display help
-    -d   -- delimiter ('\n' by default)
-    path -- path to target text file
+    -t   -- ping timeout in seconds
+    path -- path to input file
+    servers -- path to servers .json file
--- udp_idol.py
+++ udp_idol.py
@@ -32,6 +32,11 @@ THREAD_POOL_SIZE = 4
 
 
 class InputWrapper:
+    """
+    Wrapper for SWIG generated module, created from C shared object.
+    Creates generator object that returns packet (line), and strips '\n' at
+    the end
+    """
     def __init__(self, path):
         self._path = path
         self._is_open = False
@@ -75,6 +80,13 @@ def is_rules_apply(rules, _packet):
 
 
 class Addressant:
+    """
+    Class is used for transmitting raw UDP datagram to network host.
+    Before transmission host presence is checked by sending ICMP ping.
+    Every instance gets its unique ID. ID can be from 1 to 2^16 to match
+    range of ports available in PC.
+    ID is installed in UDPs frame source port.
+    """
     PORT_MAXVAL = 65535
     ERROR_RETVAL = -1
     SUCCESS_RETVAL = 0
@@ -85,6 +97,15 @@ class Addressant:
                  payload,
                  timeout,
                  interface=None):
+        """
+        Initializes class instance
+        :param name: Addressants name (have to match key in the servers file)
+        :param address: IPv4 address of host
+        :param payload: payload which to be sent
+        :param timeout: ping timeout in seconds
+        :param interface: interface for data transmission (will send to all
+        interfaces by default
+        """
         self.name = name
         self._id = random.randint(1, Addressant.PORT_MAXVAL)
         self._payload = payload
@@ -93,6 +114,10 @@ class Addressant:
         self._timeout = timeout
 
     def _ping(self):
+        """
+        Ping the host with ICMP. If host is unreachable prints error message.
+        :return: None on success and ERROR_RETVAL on failure
+        """
         ans, unans = sr(self._ip_frame / ICMP(),
                         iface=self._iface,
                         timeout=self._timeout,
@@ -103,6 +128,11 @@ class Addressant:
             return self.ERROR_RETVAL
 
     def _send(self):
+        """
+        Craft packet with UDP datagram and raw payload. Install ID of
+        instance in source port of UDP header. Send the packet.
+        :return: ID of instance
+        """
         datagram = (self._ip_frame /
                     UDP(dport=DATAGRAM_PORT,
                         sport=self._id) /
@@ -111,6 +141,10 @@ class Addressant:
         return self._id
 
     def start_transmit(self):
+        """
+        Ping with ICMP. If host is present send packet with payload.
+        :return: ID of instance on success, ERROR_RETVAL on failure.
+        """
         if self._ping() == self.ERROR_RETVAL:
             return ERROR_RETVAL
 
@@ -118,6 +152,21 @@ class Addressant:
 
 
 def factory(new_packet, missfit_name, general_rule, task_q, timeout):
+    """
+    Processes packet and creates instances of Addressant class based on
+    regexps located in Addressants_re.
+    Also applies rejection based on general rule, if it is provided.
+    Creates Addressant class object from packet rejected by all regexps in
+    Addressants_re.
+    All created objects of Addressant class are put to the task_q queue.
+    :param new_packet: packet to process (string)
+    :param missfit_name: name string of missfit receiver (have to match key in
+     the servers file)
+    :param general_rule: general rule for all packets to match
+    :param task_q: Queue where created objects will be put
+    :param timeout: timeout argument for Addressant
+    :return: None
+    """
     addr_obj = []
     packet_taken = 0
     general_re = re.compile(general_rule)
@@ -146,54 +195,108 @@ def factory(new_packet, missfit_name, general_rule, task_q, timeout):
 
 
 def is_all_rules_apply(_packet, rules):
+    """
+    Check is all regexps match given packet
+    :param _packet: packet to check
+    :param rules: regexp patterns tuple
+    :return: True if all regexps are matched else False
+    """
     return all(re.match(pattern, _packet) for pattern in rules)
 
 
 def get_servers(servers_path):
+    """
+    Parse JSON file that contains dictionary in form server_name:server_address
+    :param servers_path: path to file
+    :return: servers dictionary
+    """
     with (open(servers_path)) as srv:
         print("+++ Servers file loaded")
         return json.load(srv)
 
 
 class SnifferThread(threading.Thread):
+    """
+    When started as thread listens for specific packets. When specific
+    packet is present it is added to queue.
+    """
+    # TODO: make class singleton
     SNIFF_TIMEOUT_SEC = 4
 
     def __init__(self,
-                 interface,
                  result_q,
                  stop_event,
-                 destination_port=None,
-                 timeout=SNIFF_TIMEOUT_SEC,
-                 name='SnifferThread'):
+                 interface=None,
+                 name='SnifferThread',
+                 destination_port=None):
+        """
+        Initializes SnifferThread object
+        :param interface: interface to listen to (listen to all ifaces by
+        default
+        :param result_q: queue for placing sender port integer values of
+        captured UDP packets
+        :param stop_event: thread event that terminates SnifferThread
+        :param destination_port: port value for packets filter
+        :param name: thread name ('SnifferThread' by default)
+        """
         threading.Thread.__init__(self)
         self._destination_port = destination_port
         self._iface = interface
         self.__stop_event = stop_event
-        self._sniff_tim_out = timeout
         self.__name = name
         self._result_q = result_q
 
     def _sniffed_handler(self, caught_packet):
+        """
+        Handler for captured packets.
+        Puts packets to queue.
+        :param caught_packet: captured packet
+        :return: None
+        """
         self._result_q.put(caught_packet)
 
     def __filter(self, pkt):
+        """
+        Filter function for sniff()
+        :param pkt: captured packet
+        :return: True if pkt has UDP layer and UDPs layer dport is eqv to
+        self._destination_port else False
+        """
         return UDP in pkt and pkt[UDP].dport == self._destination_port
 
     def run(self):
+        """
+        While self.__stop_event isn't set thread will call sniff().
+        Exits from thread when self.__stop_event is set
+        :return: None
+        """
         while not self.__stop_event.isSet():
             sniff(lfilter=self.__filter,
                   prn=self._sniffed_handler,
                   iface=self._iface,
-                  timeout=self._sniff_tim_out)
+                  timeout=self.SNIFF_TIMEOUT_SEC)
         thread.exit()
 
 
 class ThreadPool:
+    """
+    Thread pool
+    """
     def __init__(self, task_queue, result_queue):
+        """
+        Initialize ThreadPool instance
+        :param task_queue: queue for the tasks
+        :param result_queue: queue for the results
+        """
         self._task_q = task_queue
         self._result_q = result_queue
 
     def _worker(self):
+        """
+        Tries to get task from task_q. Exits whet queue is empty.
+        Puts retval of Addressant object
+        :return: None
+        """
         print("+++ Worker started")
         while True:
             the_addressant = self._task_q.get(timeout=0.3)
@@ -205,6 +308,10 @@ class ThreadPool:
                 thread.exit()
 
     def crate_thread_pool(self):
+        """
+        Creates THREAD_POOL_SIZE threads and starts them
+        :return: None
+        """
         for thr_num in range(THREAD_POOL_SIZE):
             threading.Thread(target=self._worker,
                              name='Thread_{}'.format(thr_num)).start()
@@ -225,9 +332,9 @@ def main(f_args):
     Servers = get_servers(f_args.servers)
 
     sniffer_kill_event = threading.Event()
-    my_sniffer = SnifferThread(INTERFACE_DEFAULT,
-                               sniff_q,
-                               sniffer_kill_event,
+    my_sniffer = SnifferThread(interface=INTERFACE_DEFAULT,
+                               result_q=sniff_q,
+                               stop_event=sniffer_kill_event,
                                destination_port=DATAGRAM_PORT)
     my_sniffer.start()
 
@@ -241,7 +348,7 @@ def main(f_args):
                     task_q,
                     timeout=f_args.timeout)
 
-    print("Please wait")
+    print("Please wait. Still processing")
     task_q.join()
 
     print("+++ Workers are dead")
@@ -259,8 +366,13 @@ def main(f_args):
 
     s = set(sniffed_list)
     diff = [x for x in sent_list if x not in s]
-    for unsent in diff:
-        print("Packet with id {} wasn't send".format(unsent))
+
+    unsent_packets = len(diff)
+    if unsent_packets:
+        for id in diff:
+            print("Packet with id {} wasn't sent".format(id))
+    else:
+        print("{} packets was successfully sent".format(len(sniffed_list)))
 
     return SUCCESS_RETVAL
 
